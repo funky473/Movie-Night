d837f3dee6a667a84a508a8138b8d5ea5a9ac474
Henry Sylvester <hsboxing19@gmail.com>
2025-05-07 16:53:03 -0400

    finish

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,11 +3,8 @@ export const metadata = {
   description: "Find the perfect movie for your evening entertainment",
 };
 
-import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
 import Workflows from "@/components/workflows";
-import Testimonials from "@/components/testimonials";
-import Cta from "@/components/cta";
 
 export default function Home() {
   return (
